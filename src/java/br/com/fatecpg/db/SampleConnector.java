23c2bed08d77cae80bcb0cc262cc173144aa3b47
Fernando <>
2018-05-21 21:47:18 -0300

    Conexao com o banco

--- src/java/br/com/fatecpg/db/SampleConnector.java
+++ src/java/br/com/fatecpg/db/SampleConnector.java
@@ -1,11 +1,25 @@
 package br.com.fatecpg.db;
 
 import java.util.ArrayList;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
     public class SampleConnector{
-        public static ArrayList<Customer> getCustomers(){
+        public static ArrayList<Customer> getCustomers() throws ClassNotFoundException, SQLException{
             ArrayList<Customer> list = new ArrayList<>();
+            Class.forName("org.apache.derby.jdbc.ClientDriver");
+            String url = "jdbc:derby://localhost:1527/sample";
+            String user = "app";
+            String pass = "app";
+            Connection con = DriverManager.getConnection(url, user, pass);// passou desta linha, é pq esta conectado, caso contrario algo deu errado.
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM CUSTOMER");
+            while(rs.next()){
             
+            }
             return list;
        }
     }
