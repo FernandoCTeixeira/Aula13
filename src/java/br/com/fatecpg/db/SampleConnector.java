1393d4d4551a22d7f42a25782554b6da04b1afae
a <a@a-PC>
2018-05-21 21:33:26 -0300

    Criação do objeto para comunicação do banco

---
+++ src/java/br/com/fatecpg/db/SampleConnector.java
@@ -0,0 +1,12 @@
+package br.com.fatecpg.db;
+
+import java.util.ArrayList;
+
+    public class SampleConnector{
+        public static ArrayList<Customer> getCustomers(){
+            ArrayList<Customer> list = new ArrayList<>();
+            
+            return list;
+       }
+    }
+
